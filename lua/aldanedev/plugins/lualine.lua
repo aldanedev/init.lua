7c27550f7a3ade7ed1963efaec8c5ec691affa77
Alex D. Neyra <aldanedev@gmail.com>
2023-12-02 16:01:45 -0500

    feat: changes opts lualine

--- lua/aldanedev/plugins/lualine.lua
+++ lua/aldanedev/plugins/lualine.lua
@@ -1,4 +1,25 @@
+local function aldanedev()
+  return "aldanedev"
+end
+
 return {
   "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons"
+  },
+  opts = {
+    options = {
+      globalstatus = true,
+    },
+    sections = {
+      lualine_c = { { "filename", file_status = true, path = 1 } },
+      lualine_x = {
+        { aldanedev }
+      }
+    },
+    inactive_winbar = {
+      lualine_c = { "filename" }
+    },
+  },
   config = true
 }
